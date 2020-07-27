6004e732c8338a81372dc622f384c64523a7dc06
akalya <akalyamohankr@gmail.com>
2020-07-27 01:54:42 -0400

    "Added JSON prop test date file and HTML reporter"

--- conf/conf.js
+++ conf/conf.js
@@ -21,18 +21,53 @@ exports.config = {
     defaultTimeoutInterval: 30000
   },
 
-  
+
 
   onPrepare: function () {
+
+    var jasmineReporters = require('c:/Users/samsung/AppData/Roaming/npm/node_modules/jasmine-reporters');
+    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+      consolidateAll: true,
+      savePath: './',
+      filePrefix: 'xmlresults'
+    }));
+
     var AllureReporter = require('C:/Users/samsung/AppData/Roaming/npm/node_modules/jasmine-allure-reporter');
     jasmine.getEnv().addReporter(new AllureReporter());
-    jasmine.getEnv().afterEach(function(done){
+    jasmine.getEnv().afterEach(function (done) {
       browser.takeScreenshot().then(function (png) {
         allure.createAttachment('Screenshot', function () {
           return new Buffer(png, 'base64')
         }, 'image/png')();
         done();
       })
+    });
+  },
+
+  //HTMLReport called once tests are finished
+  onComplete: function () {
+    var browserName, browserVersion;
+    var capsPromise = browser.getCapabilities();
+
+    capsPromise.then(function (caps) {
+      browserName = caps.get('browserName');
+      browserVersion = caps.get('version');
+      platform = caps.get('platform');
+
+      var HTMLReport = require('C:/Users/samsung/AppData/Roaming/npm/node_modules/protractor-html-reporter-2');
+
+      testConfig = {
+        reportTitle: 'Protractor Test Execution Report',
+        outputPath: './',
+        outputFilename: 'ProtractorTestReport',
+        screenshotPath: './screenshots',
+        testBrowser: browserName,
+        browserVersion: browserVersion,
+        modifiedSuiteName: false,
+        screenshotsOnlyOnFailure: true,
+        testPlatform: platform
+      };
+      new HTMLReport().from('xmlresults.xml', testConfig);
     });
   }
 
--- tests/AngularJS_Banking.js
+++ tests/AngularJS_Banking.js
@@ -2,13 +2,22 @@
 let homepage = require('../pages/AngularJS_Banking_homePage');
 let loginpage = require('../pages/AngularJS_Banking_BankManagerLoginPage');
 let addcustomerpage = require('../pages/AngularJS_Banking_AddCustomerPage');
+let prop = require('../testdata/prop.json');
 
-describe('demo calculator test', function () {
-    it('addition test', function () {
+describe('demo banking application test', function () {
+    it('check for work flow', function () {
         // browser.get('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login');
-        homepage.get('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login');
+        //homepage.get('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login');
+        homepage.get(prop.siteurl);
 
-       
+        // Printed Page Title in Console - XYZ Bank
+       expect(browser.getTitle()).toContain("XYZ Bank");
+       let pageTitle = browser.getTitle();
+
+       pageTitle.then(function(txt)
+       {
+           console.log("Page Title :- " + txt);
+       })
 
         // element(by.css('[ng-click="manager()"]')).click();
         homepage.bank_manager_login_button_click();
@@ -16,18 +25,31 @@ describe('demo calculator test', function () {
         //element(by.css('[ng-click="addCust()"]')).click();
         loginpage.addcustomer_button_click();
 
-        //element(by.model('fName')).sendKeys('Akalya');
-        addcustomerpage.firstname_input('Akalya');
+        //element(by.model('fName')).sendKeys('');
+        //taken value from json prop file
+        let firstname = prop.customers.firstname;
+        addcustomerpage.firstname_input(firstname);
 
-        // element(by.model('lName')).sendKeys('Mohan');
-        addcustomerpage.lastname_input('Mohan');
+        // element(by.model('lName')).sendKeys('');
+        //taken value from json prop file
+        let lastname = prop.customers.lastname;
+        addcustomerpage.lastname_input(lastname);
 
-        // element(by.model('postCd')).sendKeys('638701');
-        addcustomerpage.postcode_input('638701');
+        // element(by.model('postCd')).sendKeys('');
+        //taken value from json prop file
+        let postalcode = prop.customers.postalcode;
+        addcustomerpage.postcode_input(postalcode);
 
         //it click add customer button
         addcustomerpage.addcustomer_button_click();
        
+       let EC = protractor.ExpectedConditions;
+       browser.wait(EC.alertIsPresent(), 4000, "Alert not found");
+        
+       let alert = browser.switchTo().alert();
+      // let AlertText = alert.getText();
+      //expect(alertText).toContain("Customer Added Successfully");
+       alert.accept();
 
         browser.sleep(4000);
     });
